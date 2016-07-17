0d21d068e55df11cca6e651b9c04dbbd1858c5fa
HOANGNH <hoangnhonline@gmail.com>
2016-07-18 06:19:04 +0700

    db

--- icho.sql
+++ icho.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2016 at 06:03 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.16
+-- Generation Time: Jul 18, 2016 at 01:18 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,32 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `icho`
 --
-CREATE DATABASE IF NOT EXISTS `icho` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `icho`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `ads_url` varchar(255) NOT NULL,
+  `time_start` int(11) NOT NULL,
+  `time_end` int(11) NOT NULL,
+  `type` int(11) NOT NULL COMMENT '1 : không liên kết, 2 : trỏ đến 1 trang, 3',
+  `display_order` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `created_user` tinyint(4) NOT NULL,
+  `updated_user` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -28,8 +47,8 @@ USE `icho`;
 -- Table structure for table `cate`
 --
 
-CREATE TABLE IF NOT EXISTS `cate` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cate` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
@@ -43,9 +62,15 @@ CREATE TABLE IF NOT EXISTS `cate` (
   `status` tinyint(1) NOT NULL,
   `icon_url` varchar(255) NOT NULL,
   `image_url` varchar(255) NOT NULL,
-  `show_product` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `show_product` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cate`
+--
+
+INSERT INTO `cate` (`id`, `name`, `alias`, `slug`, `loai_id`, `display_order`, `created_user`, `updated_user`, `created_at`, `updated_at`, `is_hot`, `status`, `icon_url`, `image_url`, `show_product`) VALUES
+(1, 'dgasg', 'dsgadsg', 'dsgds', 10, 1, 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 1, '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -53,8 +78,8 @@ CREATE TABLE IF NOT EXISTS `cate` (
 -- Table structure for table `hang_sx`
 --
 
-CREATE TABLE IF NOT EXISTS `hang_sx` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `hang_sx` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
@@ -65,9 +90,8 @@ CREATE TABLE IF NOT EXISTS `hang_sx` (
   `created_user` tinyint(4) NOT NULL,
   `updated_user` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -75,22 +99,39 @@ CREATE TABLE IF NOT EXISTS `hang_sx` (
 -- Table structure for table `loai_sp`
 --
 
-CREATE TABLE IF NOT EXISTS `loai_sp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `loai_sp` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
+  `description` text,
   `icon_url` varchar(255) DEFAULT NULL,
-  `image_url` varchar(255) DEFAULT NULL,
+  `bg_color` varchar(10) DEFAULT NULL,
+  `home_style` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1: banner lớn 2:banner nho 3 banner ngang',
   `display_order` tinyint(4) NOT NULL,
   `is_hot` tinyint(1) NOT NULL,
   `status` tinyint(1) NOT NULL,
   `created_user` tinyint(4) NOT NULL,
   `updated_user` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `loai_sp`
+--
+
+INSERT INTO `loai_sp` (`id`, `name`, `alias`, `slug`, `description`, `icon_url`, `bg_color`, `home_style`, `display_order`, `is_hot`, `status`, `created_user`, `updated_user`, `created_at`, `updated_at`) VALUES
+(1, 'Server', 'Server', 'server', '', 'server-1468649211.png', '#EE484F', 1, 1, 0, 1, 1, 0, '2016-07-16 06:03:57', '2016-07-16 06:07:15'),
+(2, 'Laptop', 'Laptop', 'laptop', '', 'laptop-1468649338.png', '#EE484F', 1, 2, 0, 1, 1, 0, '2016-07-16 06:08:45', '2016-07-16 06:09:16'),
+(3, 'Desktop', 'Desktop', 'desktop', '', 'desktop-1468649638.png', '#EE484F', 1, 5, 0, 1, 1, 0, '2016-07-16 06:14:00', '2016-07-16 06:14:00'),
+(4, 'Phần mềm', 'Phan mem', 'phan-mem', '', 'phan-mem-1468650127.png', '#EE484F', 1, 4, 0, 1, 1, 0, '2016-07-16 06:21:58', '2016-07-16 06:22:13'),
+(5, 'Thiết bị mạng', 'Thiet bi mang', 'thiet-bi-mang', '', 'thiet-bi-mang-1468650315.png', '#EE484F', 1, 3, 0, 1, 1, 0, '2016-07-16 06:25:21', '2016-07-16 06:25:21'),
+(6, 'Văn phòng', 'Van phong', 'van-phong', '', 'thiet-bi-van-phong-1468650358.png', '#EE484F', 1, 7, 0, 1, 1, 0, '2016-07-16 06:26:04', '2016-07-16 06:26:04'),
+(7, 'Linh kiện', 'Linh kien', 'linh-kien', '', 'linh-phu-kien-1468650384.png', '#EE484F', 1, 8, 0, 1, 1, 0, '2016-07-16 06:26:29', '2016-07-16 06:26:29'),
+(8, 'Điện thoại', 'Dien thoai', 'dien-thoai', '', 'dien-thoai-1468650408.png', '#EE484F', 1, 9, 0, 1, 1, 0, '2016-07-16 06:26:52', '2016-07-16 06:26:52'),
+(9, 'Phụ kiện', 'Phu kien', 'phu-kien', '', 'linh-phu-kien-1468650451.png', '#EE484F', 1, 6, 0, 1, 1, 0, '2016-07-16 06:27:35', '2016-07-16 06:27:35'),
+(10, 'dfgds', 'dfgds', 'dfgdssdgas', '', 'banner-intab2-1468658562.jpg', '#EE484F', 0, 0, 0, 1, 1, 0, '2016-07-16 08:42:45', '2016-07-16 08:42:45');
 
 -- --------------------------------------------------------
 
@@ -98,8 +139,8 @@ CREATE TABLE IF NOT EXISTS `loai_sp` (
 -- Table structure for table `loai_thuoc_tinh`
 --
 
-CREATE TABLE IF NOT EXISTS `loai_thuoc_tinh` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `loai_thuoc_tinh` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
   `display_order` tinyint(4) NOT NULL,
@@ -108,9 +149,8 @@ CREATE TABLE IF NOT EXISTS `loai_thuoc_tinh` (
   `created_user` tinyint(4) NOT NULL,
   `updated_user` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -118,8 +158,8 @@ CREATE TABLE IF NOT EXISTS `loai_thuoc_tinh` (
 -- Table structure for table `san_pham`
 --
 
-CREATE TABLE IF NOT EXISTS `san_pham` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `san_pham` (
+  `id` int(11) NOT NULL,
   `ma_sp` char(20) NOT NULL,
   `name` varchar(255) NOT NULL,
   `name_extend` varchar(255) NOT NULL,
@@ -147,9 +187,8 @@ CREATE TABLE IF NOT EXISTS `san_pham` (
   `created_user` tinyint(4) NOT NULL,
   `updated_user` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -157,13 +196,12 @@ CREATE TABLE IF NOT EXISTS `san_pham` (
 -- Table structure for table `sp_hinh`
 --
 
-CREATE TABLE IF NOT EXISTS `sp_hinh` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sp_hinh` (
+  `id` int(11) NOT NULL,
   `sapham_id` int(11) NOT NULL,
   `image_url` varchar(255) NOT NULL,
-  `display_order` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `display_order` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -171,11 +209,9 @@ CREATE TABLE IF NOT EXISTS `sp_hinh` (
 -- Table structure for table `sp_linhkien`
 --
 
-CREATE TABLE IF NOT EXISTS `sp_linhkien` (
+CREATE TABLE `sp_linhkien` (
   `sanpham_id` int(11) NOT NULL,
-  `linhkien_id` int(11) NOT NULL,
-  PRIMARY KEY (`sanpham_id`,`linhkien_id`),
-  UNIQUE KEY `sanpham_id` (`sanpham_id`,`linhkien_id`)
+  `linhkien_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -184,11 +220,9 @@ CREATE TABLE IF NOT EXISTS `sp_linhkien` (
 -- Table structure for table `sp_sosanh`
 --
 
-CREATE TABLE IF NOT EXISTS `sp_sosanh` (
+CREATE TABLE `sp_sosanh` (
   `sanpham_id` int(11) NOT NULL,
-  `sosanh_id` int(11) NOT NULL,
-  PRIMARY KEY (`sanpham_id`,`sosanh_id`),
-  UNIQUE KEY `sanpham_id` (`sanpham_id`,`sosanh_id`)
+  `sosanh_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -197,8 +231,8 @@ CREATE TABLE IF NOT EXISTS `sp_sosanh` (
 -- Table structure for table `thuoc_tinh`
 --
 
-CREATE TABLE IF NOT EXISTS `thuoc_tinh` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `thuoc_tinh` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
   `display_order` tinyint(4) NOT NULL,
@@ -208,10 +242,119 @@ CREATE TABLE IF NOT EXISTS `thuoc_tinh` (
   `created_user` tinyint(4) NOT NULL,
   `updated_user` tinyint(4) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cate`
+--
+ALTER TABLE `cate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hang_sx`
+--
+ALTER TABLE `hang_sx`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `loai_sp`
+--
+ALTER TABLE `loai_sp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `loai_thuoc_tinh`
+--
+ALTER TABLE `loai_thuoc_tinh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `san_pham`
+--
+ALTER TABLE `san_pham`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sp_hinh`
+--
+ALTER TABLE `sp_hinh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sp_linhkien`
+--
+ALTER TABLE `sp_linhkien`
+  ADD PRIMARY KEY (`sanpham_id`,`linhkien_id`),
+  ADD UNIQUE KEY `sanpham_id` (`sanpham_id`,`linhkien_id`);
+
+--
+-- Indexes for table `sp_sosanh`
+--
+ALTER TABLE `sp_sosanh`
+  ADD PRIMARY KEY (`sanpham_id`,`sosanh_id`),
+  ADD UNIQUE KEY `sanpham_id` (`sanpham_id`,`sosanh_id`);
+
+--
+-- Indexes for table `thuoc_tinh`
+--
+ALTER TABLE `thuoc_tinh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `cate`
+--
+ALTER TABLE `cate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `hang_sx`
+--
+ALTER TABLE `hang_sx`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `loai_sp`
+--
+ALTER TABLE `loai_sp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `loai_thuoc_tinh`
+--
+ALTER TABLE `loai_thuoc_tinh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `san_pham`
+--
+ALTER TABLE `san_pham`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sp_hinh`
+--
+ALTER TABLE `sp_hinh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `thuoc_tinh`
+--
+ALTER TABLE `thuoc_tinh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
